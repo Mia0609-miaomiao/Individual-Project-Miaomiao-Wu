db5695ff7782f046117eacbc2ca7b7039999debe
Mia0609-miaomiao <mwu7@seattleu.edu>
2026-03-20 04:40:05 -0700

    Update start.sh

--- Individual Project/Controllers/start.sh
+++ Individual Project/Controllers/start.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+# start.sh
+cd /root/image-processor
+pkill -f "server.main" 2>/dev/null
+sleep 1
+nohup python3 -m server.main 50051 > server.log 2>&1 &
+sleep 2
+cat server.log
+echo "PID: $!"
